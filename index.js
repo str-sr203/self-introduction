a8c5ba22823d6da9857f53c8f1d43dc232d1f687
str-sr203 <sora.ooshiro@e-grant.net>
2022-07-11 14:46:21 +0900

    UI調整

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //ターミナル内に一行ずつ出力
-str = ["大城礎良 (おおしろ　そら)<br>",
+let str = [
+       "大城礎良 (おおしろ　そら)<br>",
        "沖縄県<br>",
        "21歳<br>",
        "男<br>",
@@ -9,56 +10,56 @@ str = ["大城礎良 (おおしろ　そら)<br>",
        "好きな歌手 : 宇多田ヒカル<br>",
        "好きな色 : 青<br>",
        "好きな動物 : イエティ<br>",
-       "好きなアニメ : ワンピース<br>",
+       "好きなアニメ : 進撃の巨人<br>",
        "やってみたいこと : グランピング<br>",
        "行ってみたい場所 : 東京スカイツリー<br>",
-       "得意なスポーツ : バドミントン<br>",
-       "これからよろしくお願いいたします。<br>"
-      ]
+       "得意なスポーツ : バドミントン<br>"
+      ];
 
 count = 0;
-function setTextLine()
-{
+function setTextLine(){
 	if (str.length >= count) {
         text = "";
 
 	    for (i=0; i<count; i++){ 
-            text += "C:¥Users¥unknown>" + str[i] 
-        };
+            text += "C:¥Users¥unknown>" + str[i];
+        }
 
-	    test.innerHTML = text;
+	    txt.innerHTML = text;
         count++;
 
-	    setTimeout("setTextLine()",1300);
+	    setTimeout("setTextLine()",1500);
     }
 }
-// setTextLine();
+setTextLine();
 
 //カーソルの点滅
 const cursor = () => {
     target = document.getElementById("cursor");
     if (target.className == "") {
         target.className = "transparent";
-    }else {target.className = "";}
+    }else {
+        target.className = "";
+    }
     setTimeout(cursor, 550);
 } 
-// cursor();
+cursor();
 
 //モーダル表示
 const modal = document.getElementById('modal');
-const image = document.getElementById("modal-image")
+const image = document.getElementById("modal-image");
 function buttonClick(num){
     modal.style.display = 'block';
     if (num === 1){
-        image.src="image/sea.jpg"
+        image.src="image/myphoto.jpg";
     }else if(num === 2) {
-        image.src="image/sea2.jpg"
+        image.src="image/sea.jpg";
     }else if(num === 3) {
-        image.src="image/sea3.jpg"
+        image.src="image/sea2.jpg";
     }else if(num === 4) {
-        image.src="image/waterfall.jpg"
+        image.src="image/waterfall.jpg";
     }else if(num === 5) {
-        image.src="image/eisa.jpg"
+        image.src="image/eisa.jpg";
     }
 }
 
